537b6f0a084d337229d745848dd848e5732e52ef
juliisamo <jsaavedramorgado@gmail.com>
2018-04-06 22:16:57 -0300

    Add files via upload

---
+++ 00-00-Hello/hello.cpp
@@ -0,0 +1,15 @@
+/* Qué: Julian Saavedra M.
+ * TP0: hello world
+ * Quiénes: Julian Saavedra M.
+ * Cuando: 3/4/2018
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    cout << "Hello, World!";
+    return 0;
+}
+
