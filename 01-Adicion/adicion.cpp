ff8d7830de687326fb177b543d44f75d86a98e8e
juliisamo <jsaavedramorgado@gmail.com>
2018-04-19 00:55:59 -0300

    Add files via upload

---
+++ 01-Adicion/adicion.cpp
@@ -0,0 +1,16 @@
+/* Qué: Julian Saavedra M.
+ * TP1: adicion
+ * Julian Saavedra M.
+ * 18/4/2018
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int a, b;
+    cin >> a >> b ;
+    cout << a + b ;
+}
+
