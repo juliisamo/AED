a0e1b82dd8e4d3ae7149d1854e35dbcc61655db4
juliisamo <jsaavedramorgado@gmail.com>
2018-04-19 13:00:51 -0300

    Add files via upload

---
+++ 02-EjemploTipos/EjemplosTipos.cpp
@@ -0,0 +1,20 @@
+/* Qué: Julian Saavedra M.
+ * TP2: Ejemplo de tipos de datos
+ * Julian Saavedra M.
+ * 18/4/2018
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    bool a = true ;
+    char b = 'j';
+    unsigned c = 453 ;
+    int d = -54 ;
+    double e = 3.1415 ;
+    string f = "ksdf" ;
+    
+    cout << a << '\n' << b << '\n' << c << '\n' << d << '\n' << e << '\n' << f << '\n';
+}
