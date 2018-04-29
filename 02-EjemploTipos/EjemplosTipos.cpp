989d6d17bc2685323c9eb353076421f803b3878b
juliisamo <jsaavedramorgado@gmail.com>
2018-04-28 21:06:27 -0300

    correxion de tp

--- 02-EjemploTipos/EjemplosTipos.cpp
+++ 02-EjemploTipos/EjemplosTipos.cpp
@@ -5,16 +5,38 @@
  */
 
 #include <iostream>
+#include <cassert>
+
 using namespace std;
 
 int main() 
 {
     bool a = true ;
-    char b = 'j';
-    unsigned c = 453 ;
-    int d = -54 ;
-    double e = 3.1415 ;
-    string f = "ksdf" ;
+    bool b = true;
     
-    cout << a << '\n' << b << '\n' << c << '\n' << d << '\n' << e << '\n' << f << '\n';
+    char c = 'j';
+    char d = 'w';
+    
+    unsigned e = 43;
+    unsigned f = 23;
+    
+    int g = -322 ;
+    int h = -2 ;
+    
+    double i = 3.14 ;
+    double j = 3.04;
+    
+    string k = "hola";
+    string l = "mundo";
+    
+    
+    assert (a == b);
+    assert (c != d);
+    assert (e + f == 66);
+    assert (g%h == 0);
+    assert (i - j == 0.1);
+    assert (length(l) * length(k) == 16 );
+    
+     
+    //cout << a << '\n' << b << '\n' << c << '\n' << d << '\n' << e << '\n' << f << '\n';
 }
